20627084052fef9eea8273581691adca28ef46f1
Joe Muoio <jgm55@drexel.edu>
2015-03-23 20:21:05 -0400

    Adding initial game rules enforced. Slight bug occasionally onsquares.

---
+++ Assets/scripts/BlockController.cs
@@ -0,0 +1,120 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockController : MonoBehaviour {
+
+	public Sprite XPiece, OPiece;
+	private Sprite basePiece;
+	public GameObject spotlightPrefab;
+	GameObject instantiatedSpotlight;
+
+	//NOTE: Must Set these when instantiate object
+	public int x, y;
+	public enum BlockState {NUETRAL,O,X};
+	public enum BlockHighlightState {SELECTED, MOVE_TO, NUETRAL};
+
+	BlockState state = BlockState.NUETRAL;
+	BlockHighlightState highlightState = BlockHighlightState.NUETRAL;
+
+	// Use this for initialization
+	void Start () {
+		basePiece = GetComponent<SpriteRenderer>().sprite;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+		if(state == BlockState.X){
+			spriteRenderer.sprite = XPiece;
+		} else if(state == BlockState.O){
+			spriteRenderer.sprite = OPiece;
+		} else if(state == BlockState.NUETRAL){
+			spriteRenderer.sprite = basePiece;
+		} else {
+			Debug.Log("AHH BAD - STATE SET WRONG ON BLOCK");
+		}
+
+		Vector3 position = new Vector3(transform.position.x,transform.position.y, transform.position.z+spotlightPrefab.transform.position.z);
+
+		if(highlightState == BlockHighlightState.MOVE_TO){
+			//Highilight it that it can be moved
+			if(instantiatedSpotlight == null){
+				instantiatedSpotlight = GameObject.Instantiate(spotlightPrefab, position, transform.rotation) as GameObject;
+			}
+		} else if(highlightState == BlockHighlightState.SELECTED){
+			//The block is selected
+			if(instantiatedSpotlight == null){
+				instantiatedSpotlight = GameObject.Instantiate(spotlightPrefab, position, transform.rotation) as GameObject;
+			}
+		} else if(highlightState == BlockHighlightState.NUETRAL){
+			//remove highlighting
+			if(instantiatedSpotlight != null){
+				Destroy(instantiatedSpotlight);
+			}
+		} else {
+			Debug.Log("BAD - HIGHLIGHT STATE SET WRONG ON BLOCK");
+		}
+	}
+
+	void OnMouseDown(){
+		GameController gameController = FindObjectOfType<GameController>();
+		//Check for placing
+		if(highlightState == BlockHighlightState.MOVE_TO){
+			if(gameController.turn == GameController.PlayerTurn.O_TURN){
+				state = BlockState.O;
+			} else if(gameController.turn == GameController.PlayerTurn.X_TURN){
+				state = BlockState.X;
+			}
+			BlockController[] blocks = GameObject.FindObjectsOfType<BlockController>();
+			foreach(BlockController block in blocks){
+				if(block.highlightState == BlockHighlightState.SELECTED){
+					//found the move from. Remove it.
+					block.state = BlockState.NUETRAL;
+					//remove it
+					gameController.move(block.x,block.y, false);
+				}
+				//block.highlightState = BlockHighlightState.NUETRAL;
+			}
+			resetBlocks();
+			gameController.move(x,y, true);
+		} else if(state == BlockState.NUETRAL){
+			if(gameController.turn == GameController.PlayerTurn.X_TURN && gameController.xPieces > 0){
+				state = BlockState.X;
+				gameController.move(x,y, true);
+				resetBlocks();
+			} else if(gameController.turn == GameController.PlayerTurn.O_TURN && gameController.oPieces > 0){
+				state = BlockState.O;
+				gameController.move(x,y, true);
+				resetBlocks();
+			}
+		} else if(!gameController.canMove(x,y) && 
+		          ((state == BlockState.O && gameController.turn == GameController.PlayerTurn.O_TURN)
+		 ||(state == BlockState.X && gameController.turn == GameController.PlayerTurn.X_TURN))){ 
+			// Remove a piece
+			state = BlockState.NUETRAL;
+			gameController.move(x,y, true);
+			resetBlocks();
+		} else if(highlightState == BlockHighlightState.SELECTED){
+			//De-select a piece
+			foreach(BlockController block in gameController.getMoveBlocks(x,y)){
+				block.highlightState = BlockHighlightState.NUETRAL;
+			}
+			highlightState = BlockHighlightState.NUETRAL;
+		} else if((state == BlockState.X && gameController.turn == GameController.PlayerTurn.X_TURN) 
+		          || (state == BlockState.O && gameController.turn == GameController.PlayerTurn.O_TURN ) ){
+			//Select a piece
+			highlightState = BlockHighlightState.SELECTED;
+			foreach(BlockController block in gameController.getMoveBlocks(x,y)){
+				block.highlightState = BlockHighlightState.MOVE_TO;
+			}
+		}
+	}
+
+	private void resetBlocks(){
+		BlockController[] blocks = GameObject.FindObjectsOfType<BlockController>();
+		foreach(BlockController block in blocks){
+			block.highlightState = BlockHighlightState.NUETRAL;
+		}
+	}
+}
---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,258 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	public enum PlayerTurn{X_TURN, O_TURN, NUETRAL, O_WINS, X_WINS};
+
+	public PlayerTurn turn = PlayerTurn.X_TURN;
+	public GUIStyle style;
+
+	public int oPieces = 5;
+	public int xPieces = 5;
+
+	public const int BOARD_SIZE = 4;
+
+	int [][] board;
+
+	//GUI Stuff
+	Vector2 resolution;
+	float resx;
+	float resy;
+	Rect xPiecesLeftRect;
+	Rect oPiecesLeftRect;
+	Rect winLabelRect;
+
+	Vector2 piecesLeftSize = new Vector2(180,30);
+	Vector2 winLabelSize = new Vector2(180,30);
+	Vector2 xPiecesLeftPos;
+	Vector2 oPiecesLeftPos;
+	const int FONT_SIZE = 24;
+	// Use this for initialization
+	void Start () {
+
+		xPiecesLeftPos = new Vector2(1280 - piecesLeftSize.x, 20);
+		oPiecesLeftPos = new Vector2(0, FONT_SIZE);
+		
+		resolution = new Vector2(Screen.width, Screen.height);
+		resx = resolution.x/1280.0f; // 1280 is the x value of the working resolution
+		resy = resolution.y/800.0f; // 800 is the y value of the working resolution
+		xPiecesLeftRect = new Rect(xPiecesLeftPos.x*resx,xPiecesLeftPos.y*resy,piecesLeftSize.x*resx,piecesLeftSize.y*resy);
+		oPiecesLeftRect = new Rect(oPiecesLeftPos.x*resx,oPiecesLeftPos.y*resy,piecesLeftSize.x*resx,piecesLeftSize.y*resy);
+		winLabelRect = new Rect(resolution.x / 2 - winLabelSize.x,40*resy,winLabelSize.x * resx, winLabelSize.y*resy);
+
+		board = new int[BOARD_SIZE][];
+		for(int i=0; i<BOARD_SIZE; i++){
+			int[] temp = new int[BOARD_SIZE];
+			for(int j=0; j<BOARD_SIZE; j++){
+				temp[j] = 0;
+				//board[i][j] = 0;
+			}
+			board[i] = temp;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Swapped x and y to match board representation
+	public void move(int y, int x, bool updateTurn){
+
+		if(board[x][y] == 0){
+			//1 for o, 2 for x
+			if(turn == PlayerTurn.O_TURN){
+				board[x][y] = 1;
+				oPieces--;
+				if(updateTurn){
+					turn = PlayerTurn.X_TURN;
+				}
+			} else if(turn == PlayerTurn.X_TURN){
+				board[x][y] = 2;
+				xPieces--;
+				if(updateTurn){
+					turn = PlayerTurn.O_TURN;
+				}
+			}
+		} else{ // It was removed
+			board[x][y] = 0;
+			if(turn == PlayerTurn.O_TURN){
+				oPieces++;
+				if(updateTurn){
+					turn = PlayerTurn.X_TURN;
+				}
+			} else if(turn == PlayerTurn.X_TURN){
+				xPieces++;
+				if(updateTurn){
+					turn = PlayerTurn.O_TURN;
+				}
+			}
+		}
+
+		int result = checkWin(x,y);
+		if(1 == result){
+			//O wins
+			turn = PlayerTurn.O_WINS;
+		} else if(2 == result){
+			//x wins
+			turn = PlayerTurn.X_WINS;
+		}
+
+		Debug.Log("Board");
+		for(int i=0;i<BOARD_SIZE;i++){
+			Debug.Log(board[i][0]+" "+board[i][1]+" "+board[i][2]+" "+board[i][3]);
+		}
+	}
+
+	/**
+	 * Checks if the updated x/y coord leads to a win.
+	 * Returns 1 for O winning, 2 for X winning, 0 otherwise;
+	 * */
+	private int checkWin(int x, int y){
+		int piece = board[x][y];
+		if( piece == 0){
+			return 0;
+		}
+
+		//check horizontal
+		bool same = true;
+		for(int i=0;i<BOARD_SIZE;i++){
+			same = (piece==board[i][y]) && same;
+		}
+		if (same) {
+			return board[x][y];
+		}
+
+		//check Vertical
+		same = true;
+		for(int i=0;i<BOARD_SIZE;i++){
+			same = (piece==board[x][i]) && same;
+		}
+		if (same) {
+			return piece;
+		}
+
+		same = true;
+		//check for 4 corners
+		same = (piece == board[0][0] && piece == board[BOARD_SIZE-1][0] 
+		        && piece == board[0][BOARD_SIZE-1] && piece == board[BOARD_SIZE-1][BOARD_SIZE-1]);
+		if (same) {
+			return piece;
+		}
+
+		if(x-1 >= 0){
+			//check up left square
+			if(y-1 >= 0){
+				if(piece == board[x-1][y] && piece == board[x-1][y-1] 
+				        && piece == board[x][y-1]){
+					return piece;
+				}
+			} else if(y+1 < BOARD_SIZE){//check down left square
+				if(piece == board[x-1][y] && piece == board[x-1][y+1] 
+				   && piece == board[x][y+1]){
+					return piece;
+				}
+			}
+		} else if(x+1 < BOARD_SIZE){
+			//check up right square
+			if(y-1 >= 0){
+				if(piece == board[x+1][y] && piece == board[x+1][y-1] 
+				   && piece == board[x][y-1]){
+					return piece;
+				}
+			} else if(y+1 < BOARD_SIZE){//check down right square
+				if(piece == board[x+1][y] && piece == board[x+1][y+1] 
+				   && piece == board[x][y+1]){
+					return piece;
+				}
+			}
+		}
+
+		return 0;
+	}
+	/**
+	 * 
+	 * Return an ArrayList of Vector2 of x,y coordinates where can move
+	 * */
+	private ArrayList getMovePositions(int x, int y){
+		ArrayList positions = new ArrayList();
+		if(x-1 >= 0){
+			if(y-1 >= 0){
+				if(board[x-1][y-1] == 0){
+					positions.Add(new Vector2(x-1, y-1));
+				}
+			}
+			if(y+1 < BOARD_SIZE){//check down left square
+				if(board[x-1][y+1] == 0){
+					positions.Add(new Vector2(x-1, y+1));
+				}
+			}
+		}
+		if(x+1 < BOARD_SIZE){
+			//check down right square
+			if(y-1 >= 0){
+				if(board[x+1][y-1] == 0){
+					positions.Add(new Vector2(x+1, y-1));
+				}
+			}
+			if(y+1 < BOARD_SIZE){//check up right square
+				if(board[x+1][y+1] == 0){
+					positions.Add(new Vector2(x+1, y+1));
+				}
+			}
+		}
+		return positions;
+	}
+
+	/**
+	 * Return an ArrayList of blockControllers of x,y coordinates where can move
+	 * swapped x and y
+	 * */
+	public ArrayList getMoveBlocks(int y, int x){
+		ArrayList blockControllers = new ArrayList();
+		BlockController[] blocks = GameObject.FindObjectsOfType<BlockController>();
+		foreach(Vector2 blockVector in getMovePositions(x,y)){
+			foreach(BlockController block in blocks){
+				if(block.x == blockVector.y && block.y==blockVector.x){
+					blockControllers.Add(block);
+				}
+			}
+		}
+		return blockControllers;
+	}
+
+	public bool canMove(int y,int x){
+		return getMovePositions(x,y).Count != 0;
+	}
+
+	void OnGUI(){
+		//TODO: display moves left
+		style.fontSize = FONT_SIZE;
+
+		GUI.Label(xPiecesLeftRect, "X Pieces: " + xPieces.ToString(), style);
+		GUI.Label(oPiecesLeftRect, "O Pieces: " + oPieces.ToString(), style);
+		//TODO: display player turn
+		if(turn == PlayerTurn.O_WINS){
+			GUI.Label(winLabelRect, "O Wins!!!", style);
+		} else if(turn == PlayerTurn.X_WINS){
+			GUI.Label(winLabelRect, "X Wins!!!", style);
+		} else if(turn == PlayerTurn.O_TURN){
+			GUI.Label(winLabelRect, "O Turn...", style);
+		} else if(turn == PlayerTurn.X_TURN){
+			GUI.Label(winLabelRect, "X Turn...", style);
+		}
+		//TODO: display winner and lock game out
+		//TODO: add highlighting of winning path
+	}
+
+	/*public void remove(){
+		if(turn == PlayerTurn.X_TURN){
+			xPieces++;
+			turn = GameController.PlayerTurn.O_TURN;
+		} else if( turn == PlayerTurn.O_TURN){
+			oPieces++;
+			turn = GameController.PlayerTurn.X_TURN;
+		}
+	}*/
+}
