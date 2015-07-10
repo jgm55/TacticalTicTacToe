cd1a7d127bf8457415eeafa53497a66838523322
Joe Muoio <jgm55@drexel.edu>
2015-07-10 18:34:52 -0400

    Adding start of tutorial slides

---
+++ Assets/scripts/SwipeController.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwipeController : MonoBehaviour {
+
+    float sensitivity = 02f;
+    private Vector3 mouseReference;
+    private Vector3 mouseOffset;
+    private Vector3 location;
+    private bool isSliding;
+    private Vector3 clickDrag;
+
+    private bool right;
+    //private GameObject parentObject;
+
+    void Start()
+    {
+        location = Vector3.zero;
+    }
+
+    void Update()
+    {
+        //Click and drag on (hidden) cylinder to ring and fish.
+
+        clickDrag = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        if (isSliding)
+        {
+            // offset
+            mouseOffset = (Input.mousePosition - mouseReference);
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector3(mouseOffset.x * sensitivity, 0));
+
+            // move
+            //gameObject.transform.position = location;
+
+            // store mouse
+            mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        // sliding flag
+        isSliding = true;
+
+        // store mouse
+        mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        // rotating flag
+        isSliding = false;
+    }
+    /*
+    void RightSwipe()
+    {
+        Debug.Log("Right Swipe");
+        //load next gameObject
+        if (slideNumber >= tutorialSlides.Length)
+        {
+            Application.LoadLevel(startScreen);
+        }
+        else
+        {
+            slideNumber++;
+
+        }
+    }
+
+    void LeftSwipe()
+    {
+        Debug.Log("left Swipe");
+        if(slideNumber > 0){
+            slideNumber--;
+
+        }
+        else
+        {
+            //do a wiggle or something
+        }
+    }*/
+}
---
+++ Assets/scripts/TutorialSlideController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialSlideController : MonoBehaviour {
+
+    float difference = 1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Mathf.Abs(transform.parent.position.x - transform.localPosition.x) < difference)
+        {
+            this.transform.parent.position = new Vector3(transform.transform.position.x -
+                Mathf.Lerp(transform.parent.position.x - transform.localPosition.x, 0, Time.deltaTime), 0, 0);
+        }
+
+	}
+}
