7bc339b2e357e07674b2ea2a5e412e94cc679648
Jonathan Ahnert <whalehousestudios@gmail.com>
2015-07-13 17:51:14 -0400

    Button Flipping
    the play and tutorial buttons now flip into the vsCPU and csPlayer
    buttons and the functionality has been moved accordingly

---
+++ Assets/scripts/Play_Button.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play_Button : MonoBehaviour {
+	public GameObject tutorialButton;
+	void OnMouseDown(){
+		this.GetComponentInParent<Rotate>().canRotate = true;
+		tutorialButton.GetComponent<Rotate>().canRotate = true;
+	}
+}
