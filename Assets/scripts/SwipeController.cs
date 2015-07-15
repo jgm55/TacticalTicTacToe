aa9b8800f2178961bed3793c5ed464621a4eb109
Joe Muoio <jgm55@drexel.edu>
2015-07-15 16:54:58 -0400

    Adding in tutorial slides, not cool version

--- Assets/scripts/SwipeController.cs
+++ Assets/scripts/SwipeController.cs
@@ -2,40 +2,111 @@
 using System.Collections;
 
 public class SwipeController : MonoBehaviour {
+    private Vector3 lastTouchPos = new Vector3(0, 0, 0);
+    public string nextLevel;
+    public string previousLevel = "";
 
-    float sensitivity = 02f;
-    private Vector3 mouseReference;
-    private Vector3 mouseOffset;
-    private Vector3 location;
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Start");
+
+        Debug.Log("Start2");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0) && !lastTouchPos.Equals(new Vector3(0, 0, 0)))
+        {
+            if (lastTouchPos.x > Input.mousePosition.x)
+            {
+                RightSwipe();
+            }
+            else if (lastTouchPos.x < Input.mousePosition.x)
+            {
+                LeftSwipe();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            lastTouchPos = Input.mousePosition;
+        }
+    }
+
+    void RightSwipe()
+    {
+        Debug.Log("Right Swipe");
+        Application.LoadLevel(nextLevel);
+    }
+
+    void LeftSwipe()
+    {
+        Debug.Log("left Swipe");
+
+        if (previousLevel != "")
+        {
+            Application.LoadLevel(previousLevel);
+        }
+    }
+    /*float sensitivity = 100f;
+    //private Vector3 mouseOffset;
+    private Vector3 location, previousLocation;
     private bool isSliding;
     private Vector3 clickDrag;
+    private int slideNumber = 0;
+
+    private float totalDistanceClick = 0f;
+
+    public GameObject[] tutorialSlides= new GameObject[2];
 
     private bool right;
     //private GameObject parentObject;
 
     void Start()
     {
-        location = Vector3.zero;
+        location = tutorialSlides[0].transform.position;
+        previousLocation = tutorialSlides[0].transform.position;
     }
 
     void Update()
     {
         //Click and drag on (hidden) cylinder to ring and fish.
 
-        clickDrag = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        clickDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         if (isSliding)
         {
             // offset
-            mouseOffset = (Input.mousePosition - mouseReference);
+            //mouseOffset = (Input.mousePosition - mouseReference);
+            float xMove = Mathf.Lerp(location.x, clickDrag.x * sensitivity, Time.deltaTime);
+            totalDistanceClick += xMove;
 
-            GetComponent<Rigidbody2D>().AddForce(new Vector3(mouseOffset.x * sensitivity, 0));
+            location = Vector3.Lerp(location, new Vector3(xMove, location.y, location.z), Time.deltaTime);
 
             // move
-            //gameObject.transform.position = location;
+            gameObject.transform.position = location;
 
-            // store mouse
-            mouseReference = Input.mousePosition;
+            if (slideNumber + 1 < tutorialSlides.Length &&
+                totalDistanceClick > Mathf.Abs(tutorialSlides[slideNumber + 1].transform.position.x - previousLocation.x) / 2)
+            {
+                slideNumber++;
+                previousLocation = tutorialSlides[slideNumber].transform.position;
+            } else if (slideNumber - 1 >= 0 &&
+              totalDistanceClick > Mathf.Abs(tutorialSlides[slideNumber - 1].transform.position.x - previousLocation.x) / 2)
+            {
+                slideNumber--;
+                previousLocation = tutorialSlides[slideNumber].transform.position;
+            }
+        }
+        else
+        {
+            location = Vector3.Lerp(location, previousLocation, Time.deltaTime);
+            //Move back to previous position
+            gameObject.transform.position = location;
         }
     }
 
@@ -43,42 +114,13 @@ public class SwipeController : MonoBehaviour {
     {
         // sliding flag
         isSliding = true;
-
-        // store mouse
-        mouseReference = Input.mousePosition;
     }
 
     void OnMouseUp()
     {
         // rotating flag
         isSliding = false;
-    }
-    /*
-    void RightSwipe()
-    {
-        Debug.Log("Right Swipe");
-        //load next gameObject
-        if (slideNumber >= tutorialSlides.Length)
-        {
-            Application.LoadLevel(startScreen);
-        }
-        else
-        {
-            slideNumber++;
 
-        }
-    }
-
-    void LeftSwipe()
-    {
-        Debug.Log("left Swipe");
-        if(slideNumber > 0){
-            slideNumber--;
-
-        }
-        else
-        {
-            //do a wiggle or something
-        }
+        totalDistanceClick = 0f;
     }*/
 }
