3b0b08a068110e3e96ad5b6388e40a8d293ad470
Joe Muoio <jgm55@drexel.edu>
2015-07-23 17:03:24 -0400

    Adding ExitOnClickScript

---
+++ Assets/scripts/ExitOnClick.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitOnClick : MonoBehaviour {
+
+    void Update()
+    {
+        RaycastHit2D[] hits = AnimationHelper.getRayCastFromScreen(Input.mousePosition);
+        foreach (RaycastHit2D hit in hits)
+        {
+            if (hit.collider.gameObject == this)
+            {
+                OnMouseDown();
+            }
+        }
+    }
+    void OnMouseDown()
+    {
+        Debug.Log("QUITTING");
+        Application.Quit();
+    }
+}
